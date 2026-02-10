ba0bafe09945ebcbe2c5d6db7742ff1991164740
juansfierro <j.fierro.pr@gmail.com>
2026-02-09 23:43:45 -0400

    Adjustments to enemy class

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,27 +2,75 @@ extends Alive
 
 class_name Enemy
 
-var bullet_scene = preload("res://scenes/bullet.tscn")
-var player: Node2D = null
+@export var bullet_scene: PackedScene #Assign Bullet Scene in inspector
+@export var shoot_interval: float #For shooting enemies, seconds between each shot fired
+@export var min_distance_from_player: float #Minimum distance to keep from the Player
+@export var max_distance_from_player: float #Maximum distance to keep from the Player
 
-func _init(health: float = 100.0, spd: float = 50.0, dmg: float = 10.0) -> void:
+var player: Node2D = null
+var is_awake: bool = false # Enemies are asleep by default
+var shoot_timer: Timer
+var can_shoot: bool = true #Makes sure the Enemy fire interval and bullet instantiation are synced
+
+#===================================
+
+func _init(health: int = 100, spd: int = 50, dmg: int = 10) -> void:
 	super._init(health, spd, dmg)
 
 func _ready() -> void:
-	current_health = max_health
+	super._ready()
+	#Remember to add new enemies to the "enemies" group
 	player = get_tree().get_first_node_in_group("player")
 	
+	shoot_timer.wait_time = shoot_interval
+
+func wake_up() -> void: #wake up wake up wake up wake up wake up kawe up wake up wake up
+	is_awake = true
+	shoot_timer.start()
+
 func _physics_process(delta: float) -> void:
-	if player:
-		chase_player(delta)
-	move_and_slide()
+	if not is_awake or not player:
+		return
 	
+	maintain_distance()
+	
+	if can_shoot:
+		shoot()
+	
+	move_and_slide()
+
+func maintain_distance() -> void:
+	var direction = (player.global_position - global_position)
+	var distance = direction.length()
+	direction = direction.normalized()
+	
+	# Enemy too close, proceed to back away
+	if distance < min_distance_from_player:
+		velocity = -direction * speed
+	elif distance > max_distance_from_player:
+		velocity = direction * speed
+	else:
+		velocity = Vector2.ZERO
+
+func shoot() -> void:
+	if not bullet_scene:
+		return
+	can_shoot = false
+	
+	var bullet = bullet_scene.instantiate()
+	bullet.direction = (player.global_position - global_position).normalized()
+	bullet.global_position = global_position
+	
+	get_tree().root.get_node("Game").add_child(bullet)
+
 func chase_player(_delta: float) -> void:
 	var direction = (player.global_position - global_position).normalized()
 	velocity = direction * speed
 
-#func shoot() -> void:
-	#var bullet = bullet_scene.instantiate()
-	#bullet.global_position = global_position
-	#bullet.direction = player.global_position
-	#$/root/Game.add_child(bullet)
+
+func _on_shoot_timer_timeout() -> void:
+	can_shoot = true
+
+func die() -> void:
+	shoot_timer.stop()
+	super.die()
--- scripts/room.gd
+++ scripts/room.gd
@@ -2,3 +2,10 @@ extends Node2D
 
 func _on_player_detector_body_entered(body: Node2D) -> void:
 	Events.room_entered.emit(self)
+	wake_enemies()
+
+func wake_enemies() -> void:
+	for enemy in get_tree().get_nodes_in_group("enemies"):
+		if enemy.get_parent() == self:
+			enemy.wake_up()
+	
